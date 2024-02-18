bbb27f50d91a27fd541ce2b424c9949ba92cad43
Saed ebrahim <ebrahim-saed@campus.technion.ac.il>
2024-02-18 18:56:50 +0200

    0.2 update command.h

--- Commands.h
+++ Commands.h
@@ -128,6 +128,46 @@ class Command {
   // TODO: Add your extra methods if needed
 };
 
+class JobsList {
+public:
+    class JobEntry {
+        int job_id;
+        int job_pid;
+        string job_command_line;
+        jobStatus job_status;
+    public:
+        JobEntry(int jobId, int jobPid, string JobCmdLine, jobStatus status);
+        int getJobId();
+        int getJobPid();
+        string getJobCmdLine();
+        jobStatus getJobStatus();
+        void setJobStatus(jobStatus new_status){
+            this->job_status = new_status;
+        }
+        // TODO: Add your data members
+    };
+
+    int maxJobId = 0;
+    // TODO: Add your data members
+public:
+    JobsList() = default;
+    ~JobsList() = default;
+    void addJob(Command* cmd, int jobPid, jobStatus status);
+    void printJobsList();
+    void killAllJobs();
+    void removeFinishedJobs();
+    JobEntry * getJobById(int jobId);
+    void removeJobById(int jobId);
+    JobEntry * getLastJob(int* lastJobId);
+    JobEntry *getLastStoppedJob(int *jobId);
+
+    void setMaxJobId(int id);
+    int getMaxJobId();
+    vector<shared_ptr<JobEntry>> allJobs;
+    // TODO: Add extra methods or modify exisitng ones as needed
+};
+
+
 class BuiltInCommand : public Command {
 public:
     BuiltInCommand(const char* cmd_line): Command(cmd_line){}
@@ -184,44 +224,6 @@ public:
     void execute() override;
 };
 
-class JobsList {
-public:
-    class JobEntry {
-        int job_id;
-        int job_pid;
-        string job_command_line;
-        jobStatus job_status;
-    public:
-        JobEntry(int jobId, int jobPid, string JobCmdLine, jobStatus status);
-        int getJobId();
-        int getJobPid();
-        string getJobCmdLine();
-        jobStatus getJobStatus();
-        void setJobStatus(jobStatus new_status){
-            this->job_status = new_status;
-        }
-        // TODO: Add your data members
-    };
-
-    int maxJobId = 0;
-    // TODO: Add your data members
-public:
-    JobsList() = default;
-    ~JobsList() = default;
-    void addJob(Command* cmd, int jobPid, jobStatus status);
-    void printJobsList();
-    void killAllJobs();
-    void removeFinishedJobs();
-    JobEntry * getJobById(int jobId);
-    void removeJobById(int jobId);
-    JobEntry * getLastJob(int* lastJobId);
-    JobEntry *getLastStoppedJob(int *jobId);
-
-    void setMaxJobId(int id);
-    int getMaxJobId();
-    vector<shared_ptr<JobEntry>> allJobs;
-    // TODO: Add extra methods or modify exisitng ones as needed
-};
 
 class KillCommand : public BuiltInCommand {
     // TODO: Add your data members
